a3426ff12b51dd5a7b9aaa7475ce66e64ac36072
Inna Kushnir <innakushnir63@gmail.com>
2024-05-31 22:42:14 +0300

    Readme, requirements.txt

--- conftest.py
+++ conftest.py
@@ -1,33 +1,16 @@
-from contextlib import asynccontextmanager
-
 import pytest
-import asyncio
-
-from fastapi import FastAPI
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
 from main import app
-from database import Base, get_session, metadata
+from database import Base, get_session
 
 SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./test_workflow.db"
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
-
-metadata.bind = engine
-
-client = TestClient(app)
-
-
-async def get_async_session():
-    async with TestingSessionLocal() as session:
-        try:
-            yield session
-        finally:
-            await session.close()
 
 @pytest.fixture(scope="function")
 async def db():
@@ -47,3 +30,4 @@ async def async_client(db):
     app.dependency_overrides[get_session] = override_get_session
     async with AsyncClient(app=app, base_url="http://test") as ac:
         yield ac
+    app.dependency_overrides.clear()
